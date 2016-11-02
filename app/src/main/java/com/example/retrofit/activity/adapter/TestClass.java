47a6f1cdb45612bb5b7aa9c03f1d2e7d8a545f16
yubo <634966673@qq.com>
2016-11-02 13:57:22 +0800

    Test Class to Git；

--- app/src/main/java/com/example/retrofit/activity/adapter/TestClass.java
+++ app/src/main/java/com/example/retrofit/activity/adapter/TestClass.java
@@ -4,4 +4,22 @@ package com.example.retrofit.activity.adapter;
  * Created by QYB on 2016/11/2.
  */
 public class TestClass {
+    private int a;
+    private int b;
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
 }
